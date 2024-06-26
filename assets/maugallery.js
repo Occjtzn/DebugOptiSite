8966ab92a9973f157bf95ae687f41af20170d57d
Occcjtzn <thibault.courrieu@outlook.fr>
2024-06-26 12:42:00 +0200

    Push-260624
    Push apprès correction lightHouse

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -52,6 +52,7 @@
     $(".gallery-item").on("click", function() {
       if (options.lightBox && $(this).prop("tagName") === "IMG") {
         $.fn.mauGallery.methods.openLightBox($(this), options.lightboxId);
+        console.log(options)
       } else {
         return;
       }
@@ -154,7 +155,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +193,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
